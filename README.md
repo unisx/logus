57595f69e60aeeab096bbe9dcbbd148f268a36ac
Michael Li <alimy@gility.net>
2018-12-16 22:42:29 +0800

    export zap interface logus package

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# logus
-Just a wrap of Zap
+## logus
+Just a wrap of Zap, export useful interface.
+
+```$go
+package main
+
+import (
+    _ "github.com/alimy/logus"
+)
+
+func main() {
+    logus.Debug("just a wrap of zap")
+}
+```
---
+++ logus.go
@@ -0,0 +1,129 @@
+package logus
+
+import "go.uber.org/zap"
+
+var logger *zap.Logger
+
+func init() {
+	InDevelopment(zap.Development(), zap.AddCaller(), zap.AddCallerSkip(1))
+}
+
+func newDevelopmentConfig() zap.Config {
+	config := zap.NewDevelopmentConfig()
+	config.DisableCaller = true
+	return config
+}
+
+func newProductionConfig() zap.Config {
+	config := zap.NewProductionConfig()
+	config.DisableCaller = true
+	return config
+}
+
+// InDevelopment is a reasonable development logging configuration.
+// Logging is enabled at DebugLevel and above.
+//
+// It enables development mode (which makes DPanicLevel logs panic), uses a
+// console encoder, writes to standard error, and disables sampling.
+// StackTraces are automatically included on logs of WarnLevel and above.
+func InDevelopment(opts ...zap.Option) {
+	var err error
+	logger, err = newDevelopmentConfig().Build(opts...)
+	if err != nil {
+		panic(err)
+	}
+}
+
+// InProduction is a reasonable production logging configuration.
+// Logging is enabled at InfoLevel and above.
+//
+// It uses a JSON encoder, writes to standard error, and enables sampling.
+// StackTraces are automatically included on logs of ErrorLevel and above.
+func InProduction(opts ...zap.Option) {
+	var err error
+	logger, err = newProductionConfig().Build(opts...)
+	if err != nil {
+		panic(err)
+	}
+}
+
+// WithOptions applies the supplied Options.
+func WithOptions(opts ...zap.Option) {
+	logger = logger.WithOptions(opts...)
+}
+
+// WithCallerSkip increases the number of callers skipped by caller annotation
+// (as enabled by the AddCaller option).
+func WithCallerSkip(skip int) {
+	logger = logger.WithOptions(zap.AddCallerSkip(skip + 1))
+}
+
+// With creates a child logger and adds structured context to it. Fields added
+// to the child don't affect the parent, and vice versa.
+func With(fields ...zap.Field) {
+	logger = logger.With(fields...)
+}
+
+// Named adds a new path segment to the logger's name. Segments are joined by
+// periods. By default, Loggers are unnamed.
+func Named(s string) {
+	logger = logger.Named(s)
+}
+
+// Debug logs a message at DebugLevel. The message includes any fields passed
+// at the log site, as well as any fields accumulated on the logger.
+func Debug(msg string, fields ...zap.Field) {
+	logger.Debug(msg, fields...)
+}
+
+// Info logs a message at InfoLevel. The message includes any fields passed
+// at the log site, as well as any fields accumulated on the logger.
+func Info(msg string, fields ...zap.Field) {
+	logger.Info(msg, fields...)
+}
+
+// Warn logs a message at WarnLevel. The message includes any fields passed
+// at the log site, as well as any fields accumulated on the logger.
+func Warn(msg string, fields ...zap.Field) {
+	logger.Warn(msg, fields...)
+}
+
+// Error logs a message at ErrorLevel. The message includes any fields passed
+// at the log site, as well as any fields accumulated on the logger.
+func Error(msg string, fields ...zap.Field) {
+	logger.Error(msg, fields...)
+}
+
+// DPanic logs a message at DPanicLevel. The message includes any fields
+// passed at the log site, as well as any fields accumulated on the logger.
+//
+// If the logger is in development mode, it then panics (DPanic means
+// "development panic"). This is useful for catching errors that are
+// recoverable, but shouldn't ever happen.
+func DPanic(msg string, fields ...zap.Field) {
+	logger.DPanic(msg, fields...)
+}
+
+// Fatal logs a message at FatalLevel. The message includes any fields passed
+// at the log site, as well as any fields accumulated on the logger.
+//
+// The logger then calls os.Exit(1), even if logging at FatalLevel is
+// disabled.
+func Panic(msg string, fields ...zap.Field) {
+	logger.Panic(msg, fields...)
+}
+
+// Fatal logs a message at FatalLevel. The message includes any fields passed
+// at the log site, as well as any fields accumulated on the logger.
+//
+// The logger then calls os.Exit(1), even if logging at FatalLevel is
+// disabled.
+func Fatal(msg string, fields ...zap.Field) {
+	logger.Fatal(msg, fields...)
+}
+
+// Sync calls the underlying Core's Sync method, flushing any buffered log
+// entries. Applications should take care to call Sync before exiting.
+func Sync() error {
+	return logger.Sync()
+}
